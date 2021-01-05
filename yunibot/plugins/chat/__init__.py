a144d8d822aea4cc547cb459ba5610922afa8f5e
Kaitian Xie <kaitian96@outlook.com>
2021-01-05 05:04:42 +0000

    Implement hello

--- yunibot/plugins/chat/__init__.py
+++ yunibot/plugins/chat/__init__.py
@@ -1,15 +1,16 @@
-# import nonebot
 from nonebot import get_driver
+from nonebot.adapters import Bot
+from nonebot.plugin import on_keyword
+from nonebot.rule import to_me
 
 from .config import Config
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
 
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
+hello = on_keyword({"zai?", "在?", "在？", "在吗", "在么？", "在嘛", "在嘛？"}, rule=to_me())
 
-# @export.xxx
-# def some_function():
-#     pass
+
+@hello.handle()
+async def say_hello(bot: Bot):
+    await hello.finish("はい！私はいつも貴方の側にいますよ！")
