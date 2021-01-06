2646b618aa97b0b4e4c92a719153d43b602ead41
Kaitian Xie <kaitian96@outlook.com>
2021-01-06 18:05:16 +0000

    Setup DB

--- yunibot/plugins/pcr/__init__.py
+++ yunibot/plugins/pcr/__init__.py
@@ -1,14 +1,41 @@
 import pathlib
 
+import sqlalchemy
+from databases import Database
 from nonebot import get_driver
+from nonebot.adapters import Bot
 
 from .config import Settings
 
 global_config = get_driver().config
 settings = Settings(**global_config.dict())
 
-cwd = pathlib.Path.cwd()
-cache_dir = cwd.joinpath("db")
+cache_dir = pathlib.Path(settings.cache_dir).absolute()
+db_dir = cache_dir.joinpath("db")
 
-if not cache_dir.exists():
-    cache_dir.mkdir(parents=True)
+if not db_dir.exists():
+    db_dir.mkdir(parents=True)
+
+db_name = settings.pcr_db_name
+db_path = db_dir.joinpath(db_name)
+db_url = f"sqlite:///{str(db_path)}"
+
+database = Database(db_url)
+engine = sqlalchemy.create_engine(db_url)
+
+metadata = sqlalchemy.MetaData()
+clans = sqlalchemy.Table(
+    "clans",
+    metadata,
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("clan_name", sqlalchemy.String),
+    sqlalchemy.Column("server", sqlalchemy.String),
+)
+
+
+async def _on_bot_connect(bot: Bot):
+    metadata.create_all(engine)
+    await database.connect()
+
+
+get_driver().on_bot_connect(_on_bot_connect)
--- yunibot/plugins/pcr/config.py
+++ yunibot/plugins/pcr/config.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     cache_dir: str
+    pcr_db_name: str
 
     class Config:
         extra = "ignore"
