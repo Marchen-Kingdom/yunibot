9dcb8e84deedf68c1162e0e2548f6c7a62d7ee10
Kaitian Xie <kaitian96@outlook.com>
2021-01-07 03:37:14 -0800

    Support rank guides (#20)
    * Add img dir
    
    * Add rank guides

--- yunibot/plugins/pcr/__init__.py
+++ yunibot/plugins/pcr/__init__.py
@@ -3,26 +3,36 @@ import pathlib
 from nonebot import get_driver
 from nonebot.adapters import Bot, Event
 from nonebot.adapters.cqhttp.event import GroupMessageEvent
+from nonebot.adapters.cqhttp.message import MessageSegment
+from nonebot.log import logger
 from nonebot.plugin import on_command
 
 from .config import Settings
 from .db import ClanManager
 
 SERVERS = ["JP", "TC", "SC"]
+RANK_GUIDE_TYPES = ["前卫", "中卫", "后卫"]
 
 global_config = get_driver().config
 settings = Settings(**global_config.dict())
 
 cache_dir = pathlib.Path(settings.cache_dir).absolute()
 db_dir = cache_dir.joinpath("db")
+img_dir = cache_dir.joinpath("img")
 
 if not db_dir.exists():
     db_dir.mkdir(parents=True)
+if not img_dir.exists():
+    img_dir.mkdir(parents=True)
 
 db_name = settings.pcr_db_name
 db_path = db_dir.joinpath(db_name)
 db_url = f"sqlite:///{str(db_path)}"
 clan_manager = ClanManager(db_url)
+
+guide_names = settings.pcr_rank_guide_names.split(",")
+if len(guide_names) != 3:
+    logger.error(f"Expected 3 rank guide names, but got {len(guide_names)}")
 
 
 async def _on_bot_connect(bot: Bot):
@@ -92,3 +102,28 @@ async def handle_list_members(bot: Bot, event: GroupMessageEvent):
     if not nicknames:
         await list_members.finish("公会尚无成员")
     await list_members.finish("\n".join(nicknames))
+
+
+rank_guides = on_command("指南", block=True)
+
+
+@rank_guides.handle()
+async def handle_rank_guides(bot: Bot, event: GroupMessageEvent):
+    msg = str(event.get_message()).strip()
+    args = msg.split()
+    if len(args) != 1:
+        await rank_guides.finish("输入错误")
+    guide_type = args[0]
+    if guide_type not in RANK_GUIDE_TYPES:
+        await rank_guides.finish("输入错误")
+
+    if guide_type == RANK_GUIDE_TYPES[0]:
+        rank_guide = guide_names[0]
+    elif guide_type == RANK_GUIDE_TYPES[1]:
+        rank_guide = guide_names[1]
+    else:
+        rank_guide = guide_names[2]
+
+    rank_guide_path = f"file:///{img_dir.joinpath(rank_guide)}"
+    guide_img = MessageSegment.image(rank_guide_path)
+    await rank_guides.finish(guide_img, at_sender=True)
--- yunibot/plugins/pcr/config.py
+++ yunibot/plugins/pcr/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     cache_dir: str
     pcr_db_name: str
+    pcr_rank_guide_names: str
 
     class Config:
         extra = "ignore"
