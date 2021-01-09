f7b6f6302c03e6fc04226d7c6b2646e24dd221f1
Kaitian Xie <kaitian96@outlook.com>
2021-01-09 01:28:01 -0800

    Change table definitions (#28)
    * Change table names
    
    * Make fields not nullable

--- yunibot/plugins/pcr/model.py
+++ yunibot/plugins/pcr/model.py
@@ -1,24 +1,24 @@
 import sqlalchemy
 
 metadata = sqlalchemy.MetaData()
-clans = sqlalchemy.Table(
-    "clans",
+clan = sqlalchemy.Table(
+    "clan",
     metadata,
     sqlalchemy.Column("group_id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("clan_name", sqlalchemy.String),
-    sqlalchemy.Column("server", sqlalchemy.String),
+    sqlalchemy.Column("clan_name", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("server", sqlalchemy.String, nullable=False),
 )
-members = sqlalchemy.Table(
-    "members",
+member = sqlalchemy.Table(
+    "member",
     metadata,
     sqlalchemy.Column(
         "group_id",
         sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("clans.group_id"),
+        sqlalchemy.ForeignKey("clan.group_id"),
         primary_key=True,
     ),
     sqlalchemy.Column("user_id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("nickname", sqlalchemy.String),
+    sqlalchemy.Column("nickname", sqlalchemy.String, nullable=False),
 )
 challenge = sqlalchemy.Table(
     "challenge",
@@ -29,10 +29,10 @@ challenge = sqlalchemy.Table(
     sqlalchemy.Column(
         "group_id",
         sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("clans.group_id"),
+        sqlalchemy.ForeignKey("clan.group_id"),
     ),
     sqlalchemy.Column(
-        "user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("members.user_id")
+        "user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("member.user_id")
     ),
     sqlalchemy.Column("time", sqlalchemy.TIMESTAMP, nullable=False),
     sqlalchemy.Column("round", sqlalchemy.Integer, nullable=False),
